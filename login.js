62a82b7d1230aa90210255697f5649c383ba9457
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-01-12 23:43:50 +0300

    Create login.js

---
+++ login.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
+
+function LoginPage() {
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (password.length >= 6 && /[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+      setIsLoggedIn(true);
+    } else {
+      setError('Password must be at least 6 characters long and contain at least one symbol');
+    }
+  }
+
+  if (isLoggedIn) {
+    return <Redirect to='/home' />
+  }
+
+  return (
+    <div>
+      <NavigationBar />
+      <form onSubmit={handleSubmit}>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      {error && <p>{error}</p>}
+      <Footer />
+    </div>
+  );
+}
+
+export default LoginPage;
