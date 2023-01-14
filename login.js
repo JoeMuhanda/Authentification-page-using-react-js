d3a2052411eaa9a48c102b8ac1aed4eee8dcb36f
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-01-14 22:52:58 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,4 +1,18 @@
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 
+function LoginPage() {
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (password.length >= 6 && /[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+      setIsLoggedIn(true);
+    } else {
+      setError('Password must be at least 6 characters long and contain at least one symbol');
+    }
   }
 
   if (isLoggedIn) {
