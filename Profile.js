7c1680f523696327a3f7b130834db2f1663481f5
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-01-18 00:05:08 +0300

    Create Profile.js

---
+++ Profile.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+
+const ProfilePage = () => {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    // Perform an API request to get the user information
+    // and update the state with the response
+    const fetchUser = async () => {
+        const response = await fetch('/api/user/profile');
+        const data = await response.json();
+        setUser(data);
+    }
+    fetchUser();
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome, {user.name}</h1>
+      <p>Email: {user.email}</p>
+      <p>Address: {user.address}</p>
+    </div>
+  );
+};
+
+export default ProfilePage;
