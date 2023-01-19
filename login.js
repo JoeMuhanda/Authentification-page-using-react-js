617878a14f6e11fc3df043bf97b0ded2ad20793c
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-01-19 22:29:00 +0300

    Update login.js

--- login.js
+++ login.js
@@ -22,7 +22,19 @@ function LoginPage() {
   if (isLoggedIn) {
     return <Redirect to='/home' />
   }
-
+  return (
+    <div>
+      <NavigationBar />
+      <form onSubmit={handleSubmit}>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      {error && <p>{error}</p>}
+      <Footer />
+    </div>
 
   );
 
